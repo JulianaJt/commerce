abfae7ec955cd0dc4ce8af900fa734855897a072
JulianaJt <juliana.trujillov@urosario.edu.co>
2023-01-21 17:11:31 -0500

    app.js

--- app.js
+++ app.js
@@ -2,21 +2,23 @@
 const express = require("express"); //Traigo la libreria express
 const app = express(); //Utilizo express
 const path = require("path"); //traigo path
-const methodOverride = require('method-override'); //utilizar el metodo put y delete 
-const session = require('express-session');
+//---const methodOverride = require('method-override'); //utilizar el metodo put y delete 
+//---const session = require('express-session');
 // const auditoriaUnoMiddleware = require('./src/middlewares/auditoriaUNO'); //importo middleware
-const multer= require("multer");
-const {check} = require("express-validator");
+//---const multer= require("multer");
+//---const {check} = require("express-validator");
 //-------------------------IMPORTACION ENRUTADORES------------------------------------------------------
-const productsRouter = require("./src/routes/productosRouters"); //se trae el enrutador
-const usersRouters = require("./src/routes/usersRouters"); //se trae el enrutador
-const cookieParser = require("cookie-parser");
+//---const productsRouter = require("./src/routes/productosRouters"); //se trae el enrutador
+//---const usersRouters = require("./src/routes/usersRouters"); //se trae el enrutador
+//---const cookieParser = require("cookie-parser");
 //----------------MIDDLEWARES-----------------------------------------------------------------
 app.use(express.static(path.resolve(__dirname, './public')));//vuelve publica la carpeta public
 app.use(express.urlencoded({extended:false})); // utilizar el metodo POST
 app.use(express.json()); // utilizar el mtodo post
-app.use(methodOverride('_method')); //utilizar el metodo put y delete 
-app.use(session( {secret: "Este es mi secreto"} )); 
-app.use(cookieParser());
+//---app.use(methodOverride('_method')); //utilizar el metodo put y delete 
+//---app.use(session( {secret: "Este es mi secreto"} )); 
+//---app.use(cookieParser());
 
-
+//---------------------------------------------------------------------------------
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log("Servidor corriendo en el puerto "));
